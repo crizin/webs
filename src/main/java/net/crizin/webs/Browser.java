4eab252f7d23e87f43f0322a9d1e04aa23e506e6
JaeYong Lee <jaeyong.crizin@navercorp.com>
2023-12-18 15:29:59 +0900

    Update user agent version

--- src/main/java/net/crizin/webs/Browser.java
+++ src/main/java/net/crizin/webs/Browser.java
@@ -6,11 +6,11 @@ import java.util.Map;
 public enum Browser {
 	CHROME(
 		"Cache-Control: max-age=0",
-		"sec-ch-ua: \"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
+		"sec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
 		"sec-ch-ua-mobile: ?0",
 		"sec-ch-ua-platform: \"Windows\"",
 		"Upgrade-Insecure-Requests: 1",
-		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 		"Sec-Fetch-Site: none",
 		"Sec-Fetch-Mode: navigate",
@@ -20,11 +20,11 @@ public enum Browser {
 	),
 	EDGE(
 		"Cache-Control: max-age=0",
-		"sec-ch-ua: \"Not/A)Brand\";v=\"99\", \"Microsoft Edge\";v=\"115\", \"Chromium\";v=\"115\"",
+		"sec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
 		"sec-ch-ua-mobile: ?0",
 		"sec-ch-ua-platform: \"Windows\"",
 		"Upgrade-Insecure-Requests: 1",
-		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62",
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 		"Sec-Fetch-Site: none",
 		"Sec-Fetch-Mode: navigate",
