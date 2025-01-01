88928bc0c6084681e353c05415129ba43e40e2e9
JaeYong Lee <crizin@gmail.com>
2025-01-01 12:19:43 +0900

    Upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    id("jacoco")
 }
 
 group = "net.crizin"
@@ -10,17 +9,13 @@ repositories {
     mavenCentral()
 }
 
-jacoco {
-    toolVersion = "0.8.12"
-}
-
 dependencies {
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
     implementation("io.micrometer:micrometer-core:1.14.2")
-    testImplementation(platform("org.junit:junit-bom:5.11.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
-    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.assertj:assertj-core:3.27.0")
     testImplementation("org.testcontainers:testcontainers:1.20.4")
 }
 
