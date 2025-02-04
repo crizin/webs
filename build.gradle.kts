d18193a38a4d912da12256256213b2705e46af7c
JaeYong Lee <crizin@gmail.com>
2025-02-04 21:43:52 +0900

    Upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
-    implementation("io.micrometer:micrometer-core:1.14.2")
+    implementation("io.micrometer:micrometer-core:1.14.3")
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-    testImplementation("org.assertj:assertj-core:3.27.0")
+    testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.testcontainers:testcontainers:1.20.4")
 }
 
